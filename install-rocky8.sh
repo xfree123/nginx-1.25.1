e026fd405a0673aff91eacde55b5e5e6da7665ca
xfree123 <sanbangtatca022015@gmail.com>
2023-11-23 12:53:41 +0700

    Update install-rocky8.sh
    Bug fix

--- install-rocky8.sh
+++ install-rocky8.sh
@@ -68,7 +68,7 @@ cd nginx-1.25.1
 --add-dynamic-module=../headers-more-nginx-module \
 --add-dynamic-module=../echo-nginx-module \
 --add-dynamic-module=../nginx-module-vts \
---add-dynamic-module=../ngx_log_if 
+--add-module=../ngx_log_if 
 
 make
 make install
@@ -107,7 +107,7 @@ ln -s /usr/share/nginx/modules /etc/nginx/modules
 cat > "/etc/nginx/nginx.conf" << END
 
 # Server Global
-user  			nginx;
+user	nginx;
 worker_processes auto;
 worker_rlimit_nofile 20000;
 timer_resolution 100ms;
@@ -120,7 +120,6 @@ load_module "/etc/nginx/modules/ngx_http_geoip_module.so";
 load_module "/etc/nginx/modules/ngx_http_headers_more_filter_module.so";
 load_module "/etc/nginx/modules/ngx_http_set_misc_module.so";
 load_module "/etc/nginx/modules/ngx_stream_module.so";
-load_module "/etc/nginx/modules/ngx_http_aclog_bypass_module.so;
 # Worker Config
 events {
 	worker_connections 10000;
@@ -133,22 +132,22 @@ http {
 	log_format  main  '\$remote_addr - \$remote_user [\$time_local] "\$request" '
                       '\$status \$body_bytes_sent "\$http_referer" '
                       '"\$http_user_agent" "\$http_x_forwarded_for"';
-    access_log off;
+	access_log off;
 	error_log /var/log/nginx/error.log crit;
 # Mime settings
-    include			/etc/nginx/mime.types;
-    default_type		application/octet-stream;
+    	include			/etc/nginx/mime.types;
+    	default_type		application/octet-stream;
  
-	more_set_headers    "Server: Nginx";
-	more_set_headers "X-Powered-By: NGINX";
+	more_set_headers	"Server: Nginx";
+	more_set_headers 	"X-Powered-By: NGINX";
 #config for websocket	
 	map \$http_upgrade \$connection_upgrade {
-							default upgrade;
-							'' close;
+					default upgrade;
+					'' close;
 	}
 	
-	# SSL complaint
-	ssl_protocols	TLSv1 TLSv1.1 TLSv1.2;
+# SSL complaint
+	ssl_protocols		TLSv1 TLSv1.1 TLSv1.2;
 	ssl_ciphers "EECDH+ECDSA+AESGCM EECDH+aRSA+AESGCM EECDH+ECDSA+SHA384 EECDH+ECDSA+SHA256 EECDH+aRSA+SHA384 EECDH+aRSA+SHA256 EECDH+aRSA+RC4 EECDH EDH+aRSA HIGH !RC4 !aNULL !eNULL !LOW !3DES !MD5 !EXP !PSK !SRP !DSS";
 
 	ssl_prefer_server_ciphers on;
@@ -156,15 +155,15 @@ http {
 	ssl_session_timeout 1d;
 	ssl_session_tickets off;
 
-	#Cross-site-Scriping & Iframe Restrict:
+#Cross-site-Scriping & Iframe Restrict:
 	#add_header X-Frame-Options SAMEORIGIN;
 	#add_header X-Content-Type-Options nosniff;
 	#add_header X-XSS-Protection: "1; mode=block, 1; mode=block";
 
-  sendfile on;
-  sendfile_max_chunk 512k;
-  tcp_nopush on;
-  tcp_nodelay on;
+  	sendfile on;
+ 	sendfile_max_chunk 512k;
+  	tcp_nopush on;
+  	tcp_nodelay on;
 	types_hash_max_size 2048;
 	server_tokens off;
 	server_name_in_redirect off;
@@ -180,9 +179,9 @@ http {
 	output_buffers 	8 256k;
 	postpone_output 1460;
 	request_pool_size 32k;
-  connection_pool_size 512;
+  	connection_pool_size 512;
 	directio 4m;
-  client_body_buffer_size 256k;
+  	client_body_buffer_size 256k;
 	client_body_timeout 1m;
 	client_header_buffer_size 2k;
 	client_body_in_file_only off;
@@ -210,22 +209,22 @@ http {
 	gzip_min_length 10240;
 	gzip_types 
 		text/css
-        text/javascript
-        text/xml
-        text/plain
-        text/x-component
-        application/javascript
-        application/x-javascript
-        application/json
-        application/xml
-        application/rss+xml
-        application/atom+xml
-        font/truetype
-        font/opentype
-        application/vnd.ms-fontobject
-        image/svg+xml;
+       		text/javascript
+        	text/xml
+        	text/plain
+        	text/x-component
+        	application/javascript
+        	application/x-javascript
+        	application/json
+        	application/xml
+        	application/rss+xml
+        	application/atom+xml
+        	font/truetype
+        	font/opentype
+        	application/vnd.ms-fontobject
+        	image/svg+xml;
 	
-	# Cloudflare module cho nginx
+# Cloudflare module cho nginx
 	real_ip_header CF-Connecting-IP;
 	set_real_ip_from 173.245.48.0/20;
 	set_real_ip_from 103.21.244.0/22;
@@ -242,6 +241,7 @@ http {
 	set_real_ip_from 104.24.0.0/14;
 	set_real_ip_from 172.64.0.0/13;
 	set_real_ip_from 131.0.72.0/22;
+ 
 	set_real_ip_from 2400:cb00::/32;
 	set_real_ip_from 2606:4700::/32;
 	set_real_ip_from 2803:f800::/32;
